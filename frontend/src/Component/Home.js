f926e63704608e4c7721b5549553ce2d585f772a
hoagtran2002 <114356387+h04g@users.noreply.github.com>
2023-09-13 17:42:58 +0700

    Home commit

---
+++ frontend/src/Component/Home.js
@@ -0,0 +1,20 @@
+import React, { } from "react";
+
+const Home = () => {
+    return (
+        <>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h1>Hello Again</h1>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </>
+    )
+}
+
+export default Home;
